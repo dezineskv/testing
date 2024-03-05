023852095f7d1ba8ac118eade169c7eac7671eac
kimv <vidalkim17@gmail.com>
2024-03-04 18:36:50 -0800

    revised initial pre-db

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -19,7 +19,7 @@ export default async function Page() {
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
          {/* // FIX */}
           {/* <RevenueChart revenue={revenue}  /> */}
-          <LatestInvoices latestInvoices={latestInvoices} />
+          {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
       </div>
     </main>
   );
