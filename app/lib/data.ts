951adfd6e2b72591be18364b0edfbf80b8eb97d1
kimv <vidalkim17@gmail.com>
2024-03-07 19:23:32 -0800

    update to paginations

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -53,7 +53,7 @@ export async function fetchCardData() {
   noStore();
 
   try {
-    // You can probably combine these into a single SQL query
+    // can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
